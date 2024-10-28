0e9232e8985b3dcb85c4cde09ba2a4dd8772c74a
Sardorbek <studentofinha@gmail.com>
2024-10-28 22:33:38 +0500

    added some style to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,9 @@
-<footer>
-        Our footer page 
-    </footer>
+<!-- footer.php -->
+<footer style="background-color: #333; color: #fff; padding: 20px; text-align: center;">
+    <p>&copy; <?php echo date("Y"); ?> Our Store. All rights reserved.</p>
+    <p>
+        <a href="privacy.php" style="color: #fff; text-decoration: none; margin-right: 15px;">Privacy Policy</a>
+        <a href="terms.php" style="color: #fff; text-decoration: none; margin-right: 15px;">Terms of Service</a>
+        <a href="contact.php" style="color: #fff; text-decoration: none;">Contact Us</a>
+    </p>
+</footer>
